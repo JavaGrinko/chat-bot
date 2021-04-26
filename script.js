67172745efaa1c37baefacc79938eeb8a7b9a0a4
Artem Grinko <asgrinko@alfabank.ru>
2021-04-26 16:34:33 +0300

    диалог с ботом

---
+++ script.js
@@ -0,0 +1,74 @@
+let messagesElement;
+let messages = [];
+
+window.onload = function() {
+    console.log('Скрипты подключены');
+    messagesElement = document.getElementById("messages");
+    let submit = document.getElementById("submit");
+    let textInput = document.getElementById("text");
+    submit.onclick = function() {
+        let text = textInput.value;
+        if (text) {
+            messages.push({
+                text,
+                author: 'me'
+            });
+            updateMessageList();
+            robotAnswer(text);
+            textInput.value = "";
+            textInput.focus();
+        }
+    }
+    textInput.onkeydown = function(event) {
+        if (event.key === 'Enter') {
+            submit.click();
+        }
+    }
+}
+
+const scenario = [
+    {include: "привет", answer: "И вам доброго дня! О чем поговорим?"},
+    {include: "как дела", answer: "Зарплата хорошая. Маленькая, но хорошая."},
+    {include: "", answer: "К такому разговору меня жизнь не готовила"}
+];
+
+function robotAnswer(text) {
+    setTimeout(() => {
+        for (let s of scenario) {
+            if (text.toLowerCase().includes(s.include)) {
+                messages.push({
+                    text: s.answer,
+                    author: "robot"
+                });
+                break;
+            }
+        }
+        updateMessageList();
+    }, 1000) // типа думает секунду
+}
+
+function updateMessageList() {
+    messagesElement.innerText = "";
+    messages.forEach(m => {
+        if (m.author === "me") {
+            let div = document.createElement("div");
+            div.classList.add("message");
+            div.innerHTML = m.text;
+            messagesElement.appendChild(div);
+        } else {
+            let div = document.createElement("div");
+            div.classList.add("message");
+            div.classList.add("answer");
+            div.innerHTML = m.text;
+            messagesElement.appendChild(div);
+        }
+    });
+    updateScroll();
+}
+
+
+function updateScroll(){
+    var element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
+}
+
